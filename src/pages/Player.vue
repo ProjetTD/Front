229200be285fd35354cf84e4045d2303f396385a
Swixos <erazion.swixo@gmail.com>
2024-02-06 16:28:28 +0100

    Refactor alien spawning logic and add alien images

--- src/pages/Player.vue
+++ src/pages/Player.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-if="user.name !== undefined && user.name" class="h-screen w-full flex items-center justify-center flex-col gap-6 mt-6 mb-2">
+  <div v-if="user.name !== undefined && user.name" class="h-screen w-full flex items-center justify-center flex-col gap-6 mt-2 mb-2">
     <div class="flex flex-col items-center justify-center h-full w-full gap-6"> <!--MAIN-->
-      <div class="flex flex-col lg:flex-row items-center justify-center gap-3 mt-28 lg:mt-0">
+      <div class="flex flex-col lg:flex-row items-center justify-center gap-3 mt-10 lg:mt-0">
         <!-- Informations sur le joueur -->
         <div class="flex flex-row items-center justify-center gap-2 border-white border-[1px] bg-black/95 rounded-xl px-3 py-2">
           <img title="Pseudo" class="h-6 w-6" src="@/assets/images/player/account.svg">
@@ -29,9 +29,13 @@
         </div>
       </div>
       <div class="flex flex-row items-center justify-center mt-6"> 
-        <div class="flex flex-col justify-center ml-6">
+        <div class="flex flex-col justify-center">
           <div v-for="row in 5" :key="row" class="flex">
-            <div v-for="col in 10" :key="col" class="w-12 h-12 border-white border-[1px] rounded-lg bg-green-500 m-1"></div>
+            <div v-for="col in 10" :key="col" class="flex items-center justify-center w-20 h-20 even:border-white odd:border-black border-[1px] rounded-lg m-1 even:bg-black odd:bg-white">
+              <div v-if="col === aliensColumn && row === 1" :class="'w-16 h-16 flex items-center justify-center'">
+                <img v-if="aliens.length > 0" :src="getAlienImage(aliens[0].name)" class="-scale-x-[1] w-16 h-16" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -41,13 +45,17 @@
 
 <script>
 import store from '@/store';
+import Normal from '@/assets/images/player/alien_Normal.png';
+import Runner from '@/assets/images/player/alien_Runner.png';
+import Tank from '@/assets/images/player/alien_Tank.png';
 
 export default {
   name: 'Player',
   components: {},
   data() {
     return {
-      aliens: []
+      aliens: [],
+      aliensColumn: 10
     }
   },
   computed: {
@@ -62,6 +70,21 @@ export default {
     }
   },
   methods: {
+    getAlienImage(alienName) {
+      switch (alienName) {
+        case 'Normal':
+          return Normal;
+        case 'Runner':
+          return Runner;
+        case 'Tank':
+          return Tank;
+        default:
+          return "";
+      }
+    },
+    getRandomRow() {
+      return Math.floor(Math.random() * 5);
+    },
     getCookie(name) {
       const cookies = document.cookie.split('; ');
       for (const cookie of cookies) {
@@ -77,37 +100,39 @@ export default {
         const response = await this.$api.get('/aliens');
         const alienTypes = response.data;
 
-        let aliens = [];
-
-        // Spawn aliens based on probabilities
         for (const alienType of alienTypes) {
           const aliensToSpawn = alienType.probability * this.level.number_of_alien / 100;
 
-          console.log('Alientospawn', aliensToSpawn);
+          console.log('Aliens to spawn:', aliensToSpawn);
 
           for (let i = 0; i < aliensToSpawn; i++) {
-            aliens.push(alienType);
+            this.aliens.push(alienType);
           }
         }
 
-        console.log('Aliens:', aliens);
-
-        // Shuffle the array to ensure randomness
-        for (let i = 10; i < aliens.length; i++) {
-          const randomIndex = Math.floor(Math.random() * (aliens.length - 10)) + 10;
-          const temp = aliens[i];
-          aliens[i] = aliens[randomIndex];
-          aliens[randomIndex] = temp;
+        for (let i = 10; i < this.aliens.length; i++) {
+          const randomIndex = Math.floor(Math.random() * (this.aliens.length - 10)) + 10;
+          const temp = this.aliens[i];
+          this.aliens[i] = this.aliens[randomIndex];
+          this.aliens[randomIndex] = temp;
         }
 
-        // Trim the array to match the level's number_of_alien
-        aliens = aliens.slice(0, this.level.number_of_alien);
-
-        this.aliens = aliens;
+        const interval = setInterval(() => {
+          if (this.aliens.length > 0) {
+            this.aliensColumn -= 1;
+            if (this.aliensColumn === 0) {
+              this.aliensColumn = 10;
+            }
+            if(this.aliensColumn === 10) {
+              this.aliens.shift();
+              this.level.number_of_alien = this.aliens.length;
+            }
+          } else {
+            clearInterval(interval);
+          }
+        }, 500);
       } catch (error) {
         console.error('Erreur lors de la récupération des aliens:', error);
-        // Handle error
-        return [];
       }
     }
   },
